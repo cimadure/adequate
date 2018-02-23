e78911b64976d978d0310f2582ecee6e83ae7a17
user <ronan.cimadure@gmail.com>
2018-02-22 23:20:14 -0500

    messy

--- main.py
+++ main.py
@@ -1,121 +1,43 @@
 from os.path import dirname, join
 
-import numpy as np
-import pandas.io.sql as psql
-import sqlite3 as sql
+import pandas as pd
 
-import bokeh
-bokeh.sampledata.download()
-
-from bokeh.plotting import figure
-from bokeh.layouts import layout, widgetbox
-from bokeh.models import ColumnDataSource, HoverTool, Div
-from bokeh.models.widgets import Slider, Select, TextInput
+from bokeh.layouts import row, widgetbox
+from bokeh.models import ColumnDataSource, CustomJS
+from bokeh.models.widgets import RangeSlider, Button, DataTable, TableColumn, NumberFormatter
 from bokeh.io import curdoc
-from bokeh.sampledata.movies_data import movie_path
 
-conn = sql.connect(movie_path)
-query = open(join(dirname(__file__), 'query.sql')).read()
-movies = psql.read_sql(query, conn)
+df = pd.read_csv(join(dirname(__file__), 'data/salary_data.csv'))
 
-movies["color"] = np.where(movies["Oscars"] > 0, "orange", "grey")
-movies["alpha"] = np.where(movies["Oscars"] > 0, 0.9, 0.25)
-movies.fillna(0, inplace=True)  # just replace missing values with zero
-movies["revenue"] = movies.BoxOffice.apply(lambda x: '{:,d}'.format(int(x)))
-
-with open(join(dirname(__file__), "razzies-clean.csv")) as f:
-    razzies = f.read().splitlines()
-movies.loc[movies.imdbID.isin(razzies), "color"] = "purple"
-movies.loc[movies.imdbID.isin(razzies), "alpha"] = 0.9
-
-axis_map = {
-    "Tomato Meter": "Meter",
-    "Numeric Rating": "numericRating",
-    "Number of Reviews": "Reviews",
-    "Box Office (dollars)": "BoxOffice",
-    "Length (minutes)": "Runtime",
-    "Year": "Year",
-}
-
-desc = Div(text=open(join(dirname(__file__), "description.html")).read(), width=800)
-
-# Create Input controls
-reviews = Slider(title="Minimum number of reviews", value=80, start=10, end=300, step=10)
-min_year = Slider(title="Year released", start=1940, end=2014, value=1970, step=1)
-max_year = Slider(title="End Year released", start=1940, end=2014, value=2014, step=1)
-oscars = Slider(title="Minimum number of Oscar wins", start=0, end=4, value=0, step=1)
-boxoffice = Slider(title="Dollars at Box Office (millions)", start=0, end=800, value=0, step=1)
-genre = Select(title="Genre", value="All",
-               options=open(join(dirname(__file__), 'genres.txt')).read().split())
-director = TextInput(title="Director name contains")
-cast = TextInput(title="Cast names contains")
-x_axis = Select(title="X Axis", options=sorted(axis_map.keys()), value="Tomato Meter")
-y_axis = Select(title="Y Axis", options=sorted(axis_map.keys()), value="Number of Reviews")
-
-# Create Column Data Source that will be used by the plot
-source = ColumnDataSource(data=dict(x=[], y=[], color=[], title=[], year=[], revenue=[], alpha=[]))
-
-hover = HoverTool(tooltips=[
-    ("Title", "@title"),
-    ("Year", "@year"),
-    ("$", "@revenue")
-])
-
-p = figure(plot_height=600, plot_width=700, title="", toolbar_location=None, tools=[hover])
-p.circle(x="x", y="y", source=source, size=7, color="color", line_color=None, fill_alpha="alpha")
-
-
-def select_movies():
-    genre_val = genre.value
-    director_val = director.value.strip()
-    cast_val = cast.value.strip()
-    selected = movies[
-        (movies.Reviews >= reviews.value) &
-        (movies.BoxOffice >= (boxoffice.value * 1e6)) &
-        (movies.Year >= min_year.value) &
-        (movies.Year <= max_year.value) &
-        (movies.Oscars >= oscars.value)
-    ]
-    if (genre_val != "All"):
-        selected = selected[selected.Genre.str.contains(genre_val)==True]
-    if (director_val != ""):
-        selected = selected[selected.Director.str.contains(director_val)==True]
-    if (cast_val != ""):
-        selected = selected[selected.Cast.str.contains(cast_val)==True]
-    return selected
-
+source = ColumnDataSource(data=dict())
 
 def update():
-    df = select_movies()
-    x_name = axis_map[x_axis.value]
-    y_name = axis_map[y_axis.value]
+    current = df[(df['salary'] >= slider.value[0]) & (df['salary'] <= slider.value[1])].dropna()
+    source.data = {
+        'name'             : current.name,
+        'salary'           : current.salary,
+        'years_experience' : current.years_experience,
+    }
 
-    p.xaxis.axis_label = x_axis.value
-    p.yaxis.axis_label = y_axis.value
-    p.title.text = "%d movies selected" % len(df)
-    source.data = dict(
-        x=df[x_name],
-        y=df[y_name],
-        color=df["color"],
-        title=df["Title"],
-        year=df["Year"],
-        revenue=df["revenue"],
-        alpha=df["alpha"],
-    )
+slider = RangeSlider(title="Max Salary", start=10000, end=110000, value=(10000, 50000), step=1000, format="0,0")
+slider.on_change('value', lambda attr, old, new: update())
 
-controls = [reviews, boxoffice, genre, min_year, max_year, oscars, director, cast, x_axis, y_axis]
-for control in controls:
-    control.on_change('value', lambda attr, old, new: update())
+button = Button(label="Download", button_type="success")
+button.callback = CustomJS(args=dict(source=source),
+                           code=open(join(dirname(__file__), "download.js")).read())
 
-sizing_mode = 'fixed'  # 'scale_width' also looks nice with this example
+columns = [
+    TableColumn(field="name", title="Employee Name"),
+    TableColumn(field="salary", title="Income", formatter=NumberFormatter(format="$0,0.00")),
+    TableColumn(field="years_experience", title="Experience (years)")
+]
 
-inputs = widgetbox(*controls, sizing_mode=sizing_mode)
-l = layout([
-    [desc],
-    [inputs, p],
-], sizing_mode=sizing_mode)
+data_table = DataTable(source=source, columns=columns, width=800)
 
-update()  # initial load of the data
+controls = widgetbox(slider, button)
+table = widgetbox(data_table)
 
-curdoc().add_root(l)
-curdoc().title = "Movies"
+curdoc().add_root(row(controls, table))
+curdoc().title = "Export CSV"
+
+update()
